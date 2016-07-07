29d1f4354a14bbe0cc636b261303859aca9a29a1
Sydney Young <sydney.young@yale.edu>
2016-07-07 09:48:06 -0400

    Revert "Revert "really unfinished""
    This reverts commit c5c262d7f866c114463b25e9054a09f38c5abb86.

--- spec/support/equipment_model_helper.rb
+++ spec/support/equipment_model_helper.rb
@@ -1,24 +1,24 @@
 require 'spec_helper'
 
-module EquipmentModelHelper
+module EquipmentModelMocker
   # traits are the private methods defined in this module
   # a findable model with a category is created with
   # traits: [:findable, [:with_category, cat: cat]]
-  def mock_eq_model(traits: [], **attrs)
+  def self.one(traits: [], **attrs)
     instance_spy('EquipmentModel', **attrs).tap do |model|
       traits.each { |trait, *args| send(trait, model, *args) }
     end
   end
 
-  def mock_eq_models(count: 1, traits: [], **attrs)
-    Array.new(count) { mock_eq_model(traits: traits, **attrs) }
+  def self.array(count: 1, traits: [], **attrs)
+    Array.new(count) { one(traits: traits, **attrs) }
   end
 
-  def stub_collection_methods(models)
+  def self.stub__collection_methods(models)
     allow(EquipmentModel).to receive(:all).and_return(models)
   end
 
-  def mock_category(**attrs)
+  def self.category(**attrs)
     instance_spy('Category', **attrs).tap do |cat|
       allow(Category).to receive(:find).and_return(cat)
     end
@@ -26,14 +26,14 @@ module EquipmentModelHelper
 
   private
 
-  def findable(model)
+  def self.findable(model)
     id = FactoryGirl.generate(:unique_id)
     allow(model).to receive(:id).and_return(id)
     allow(EquipmentModel).to receive(:find).with(id).and_return(model)
     allow(EquipmentModel).to receive(:find).with(id.to_s).and_return(model)
   end
 
-  def with_category(model, cat: nil)
+  def self.with_category(model, cat: nil)
     cat ||= mock_category
     allow(model).to receive(:category).and_return(cat)
     models = if cat.equipment_models.is_a? Array
