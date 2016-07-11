9e1c0481c47cc08bcbb8a73b6f016b903a7cdcd0
Sydney Young <sydney.young@yale.edu>
2016-07-11 09:59:34 -0400

    pull down various helpers from other test refactor branches

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -11,4 +11,17 @@ module ControllerHelpers
   def user_signed_in?
     !current_user.nil?
   end
+
+  def mock_user(role = :user, **attrs)
+    spy('user', **FactoryGirl.attributes_for(role), **attrs)
+  end
+
+  def mock_user_sign_in(user = mock_user)
+    allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+    # necessary because otherwise ApplicationController#app_setup_check fails
+    allow(User).to receive(:count).and_return(1)
+    # necessary for permissions to work
+    allow(ApplicationController).to receive(:current_user).and_return(user)
+    allow(Ability).to receive(:new).and_return(Ability.new(user))
+  end
 end
---
+++ spec/support/reservation_helper.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+module ReservationHelper
+  def mock_reservation(factory: nil, **attrs)
+    attrs = FactoryGirl.attributes_for(factory).merge attrs if factory
+    instance_spy('reservation', **attrs)
+  end
+
+  def stub_collection_methods(res, *scopes)
+    allow(Reservation).to receive(:all).and_return(res)
+    scopes.each { |s| allow(Reservation).to receive(s).and_return(res) }
+  end
+
+  # NOT to be used with the above method
+  # skips every query but the last one, which will return the given array of res
+  def stub_scope_chain(res, *chain)
+    final = chain.pop
+    chain.each do |scope|
+      allow(Reservation).to receive(scope).and_return(Reservation)
+    end
+    allow(Reservation).to receive(final).and_return(res)
+  end
+end
