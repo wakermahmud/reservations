f9829c8160678a67fee471c4ce1f190c73f41faa
Sydney Young <sydney.young@yale.edu>
2016-07-20 10:47:09 -0400

    restore controller helpers; shouldn't have been touched on this branch

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -1,3 +1,14 @@
 # some basic helpers to simulate devise controller methods in specs
 module ControllerHelpers
+  def current_user
+    user_session_info =
+      response.request.env['rack.session']['warden.user.user.key']
+    return unless user_session_info
+    user_id = user_session_info[0][0]
+    User.find(user_id)
+  end
+
+  def user_signed_in?
+    !current_user.nil?
+  end
 end
